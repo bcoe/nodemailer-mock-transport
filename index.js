7add50f73b86e089d0daa1d62f5ff575421a750d
Benjamin Coe <ben@npmjs.com>
2016-06-02 18:26:38 -0700

    chore: standardized code

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-var packageData = require('./package.json');
-var userValidate = require('npm-user-validate');
+var packageData = require('./package.json')
+var userValidate = require('npm-user-validate')
 
-var MockTransport = function(options) {
-  this.options = options || {};
-  this.sentMail = [];
-  this.name = 'Mock';
-  this.version = packageData.version;
-};
-
-function validate(addr) {
-  try {
-    err = userValidate.email(addr);
-    if(err != null) return err;
-  }
-  catch (err) {
-    return new Error("Error validating address.");
-  }
-  return null;
+var MockTransport = function (options) {
+  this.options = options || {}
+  this.sentMail = []
+  this.name = 'Mock'
+  this.version = packageData.version
 }
 
-MockTransport.prototype.send = function(mail, callback) {
+function validate (addr) {
+  try {
+    var err = userValidate.email(addr)
+    if (err != null) return err
+  } catch (_err) {
+    return new Error('Error validating address.')
+  }
+  return null
+}
+
+MockTransport.prototype.send = function (mail, callback) {
+  var err
+
   if (!mail.data.to) {
-    return callback(new Error('I need to know who this email is being sent to :-('));
+    return callback(new Error('I need to know who this email is being sent to :-('))
   }
 
-  if(Array.isArray(mail.data.to)) {
-    for(var i = 0; i < mail.data.to.length; i++) {
-      var addr = mail.data.to[i];
-      var err = validate(addr);
-      if(err != null) {
-        return callback(err);
+  if (Array.isArray(mail.data.to)) {
+    for (var i = 0; i < mail.data.to.length; i++) {
+      var addr = mail.data.to[i]
+      err = validate(addr)
+      if (err != null) {
+        return callback(err)
       }
     }
-  }
-  else {
-    var err = validate(mail.data.to);
-    if(err != null) {
-      return callback(err);
+  } else {
+    err = validate(mail.data.to)
+    if (err != null) {
+      return callback(err)
     }
   }
 
-  this.sentMail.push(mail);
-  return callback();
-};
+  this.sentMail.push(mail)
+  return callback()
+}
 
-module.exports = function(options) {
-  return new MockTransport(options);
-};
+module.exports = function (options) {
+  return new MockTransport(options)
+}
--- test/test-mock-transport.js
+++ test/test-mock-transport.js
@@ -1,97 +1,99 @@
-var chai = require('chai').should(),
-  nodemailer = require('nodemailer'),
-  mockTransport = require('../index');
+/* global it, describe */
 
-describe('mock-transport', function() {
-  it('should store configuration options so that they can be asserted against', function() {
+var nodemailer = require('nodemailer')
+var mockTransport = require('../index')
+
+require('chai').should()
+
+describe('mock-transport', function () {
+  it('should store configuration options so that they can be asserted against', function () {
     var transport = mockTransport({
       foo: 'bar'
-    });
-    transport.options.foo.should.equal('bar');
-  });
+    })
+    transport.options.foo.should.equal('bar')
+  })
 
-  it('should store emails sent with nodemailer, so that they can be asserted against', function() {
+  it('should store emails sent with nodemailer, so that they can be asserted against', function () {
     var transport = mockTransport({
       foo: 'bar'
-    });
+    })
 
-    var transporter = nodemailer.createTransport(transport);
+    var transporter = nodemailer.createTransport(transport)
 
     transporter.sendMail({
       from: 'sender@address.com',
       to: 'receiver@address.com',
       subject: 'hello',
       text: 'hello world!'
-    });
+    })
 
-    transport.sentMail.length.should.equal(1);
-    transport.sentMail[0].data.to.should.equal('receiver@address.com');
-    transport.sentMail[0].message.content.should.equal('hello world!');
-  });
+    transport.sentMail.length.should.equal(1)
+    transport.sentMail[0].data.to.should.equal('receiver@address.com')
+    transport.sentMail[0].message.content.should.equal('hello world!')
+  })
 
   it('should return an error and not send an email if there is no `to` in the mail data object', function () {
     var transport = mockTransport({
       foo: 'bar'
-    });
+    })
 
-    var transporter = nodemailer.createTransport(transport);
+    var transporter = nodemailer.createTransport(transport)
 
     transporter.sendMail({
       from: 'sender@address.com',
       subject: 'hello',
       text: 'hello world!'
-    });
+    })
 
-    transport.sentMail.length.should.equal(0);
-  });
+    transport.sentMail.length.should.equal(0)
+  })
 
   it('should return an error and not send an email if the `to` email address is invalid', function () {
     var transport = mockTransport({
       foo: 'bar'
-    });
+    })
 
-    var transporter = nodemailer.createTransport(transport);
+    var transporter = nodemailer.createTransport(transport)
 
     transporter.sendMail({
       to: 'lolbad@email',
       from: 'sender@address.com',
       subject: 'hello',
       text: 'hello world!'
-    });
+    })
 
-    transport.sentMail.length.should.equal(0);
-  });
+    transport.sentMail.length.should.equal(0)
+  })
 
-  it('should allow "to" to be an array of addresses', function(){
+  it('should allow "to" to be an array of addresses', function () {
     var transport = mockTransport({
       foo: 'bar'
-    });
-    var transporter = nodemailer.createTransport(transport);
-    var to = ['receiver@address.com','receiver2@address.com']; 
+    })
+    var transporter = nodemailer.createTransport(transport)
+    var to = ['receiver@address.com', 'receiver2@address.com']
     transporter.sendMail({
       from: 'sender@address.com',
       to: to,
       subject: 'hello',
       text: 'hello world!'
-    });
-    transport.sentMail.length.should.equal(1);
-    transport.sentMail[0].data.to.should.eql(to);
-    transport.sentMail[0].message.content.should.equal('hello world!');
+    })
+    transport.sentMail.length.should.equal(1)
+    transport.sentMail[0].data.to.should.eql(to)
+    transport.sentMail[0].message.content.should.equal('hello world!')
+  })
 
-  });
-  it('should not send an email if "to" is array and element isn\'t valid', function(){
+  it('should not send an email if "to" is array and element isn\'t valid', function () {
     var transport = mockTransport({
       foo: 'bar'
-    });
-    var transporter = nodemailer.createTransport(transport);
-    var to = ['receiver@address.com', 34]; 
+    })
+    var transporter = nodemailer.createTransport(transport)
+    var to = ['receiver@address.com', 34]
     transporter.sendMail({
       from: 'sender@address.com',
       to: to,
       subject: 'hello',
       text: 'hello world!'
-    });
-    transport.sentMail.length.should.equal(0);
-
-  });
-});
+    })
+    transport.sentMail.length.should.equal(0)
+  })
+})
